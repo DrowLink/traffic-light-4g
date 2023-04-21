a5d0de04231b5592aae17de9daa1047e74e20746
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-04-21 21:48:17 +0000

    finished

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+
+
+export const SecondsCounter = (props) => {
+   
+   return (
+        <>
+            
+
+            <div className="row bg-dark justify-content-around">
+                
+                <div className="card text-bg-dark text-center" style={{width: "10rem", minHeight: "10rem"}}>
+                            
+                    <p className="card-text my-auto text-center" style={{fontSize: "4rem"}}>🕛</p>
+                        
+                                
+                </div>
+                <SecondsNumber second={parseInt(props.timer / 100000 % 10)}/>
+                <SecondsNumber second={parseInt(props.timer / 10000 % 10)}/>
+                <SecondsNumber second={parseInt(props.timer / 1000 % 10)}/>
+                <SecondsNumber second={parseInt(props.timer / 100 % 10)}/>
+                <SecondsNumber second={parseInt(props.timer / 10 % 10)}/>
+                <SecondsNumber second={parseInt(props.timer % 10)}/>
+                    
+            </div>
+        </>
+           
+   );
+
+};
+
+
+export const SecondsNumber = (props) =>{
+
+    return(
+
+            <>
+
+                    <div className="card text-bg-dark text-center" style={{width: "10rem", minHeight: "10rem"}}>
+                            
+                        <p className="card-text my-auto" style={{fontSize: "10rem"}}>{props.second}</p>
+                        
+                            
+                    </div>
+                    
+            </>
+    )
+
+};
+
+SecondsNumber.propTypes = {
+    second: PropTypes.number
+}
+
+
