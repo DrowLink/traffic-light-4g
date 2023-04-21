2002bb3839a09cf9ab0e5844c565149d2eafca7d
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-04-21 21:21:54 +0000

    not finished yet

---
+++ src/js/component/secondCounter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '/workspace/react-hello/src/styles/index.css'
+
+const SecondCounter = () => {
+
+    
+
+
+    return (
+        <>
+            <div class="container m-5 p-2">
+                <div className="card text-bg-dark m-1 p-2 text-center ">🕛</div>
+                <div className="card text-bg-dark m-1 p-2 text-center">0</div>
+                <div className="card text-bg-dark m-1 p-2 text-center">0</div>
+                <div className="card text-bg-dark m-1 p-2 text-center">0</div>
+                <div className="card text-bg-dark m-1 p-2 text-center">0</div>
+                <div className="card text-bg-dark m-1 p-2 text-center">0</div>
+                <div className="card text-bg-dark m-1 p-2 text-center">0</div>
+            </div>
+        </>
+    )
+}
+
+export default SecondCounter
