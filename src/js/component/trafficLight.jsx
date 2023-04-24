a03330f521b9436956f6900603e88a7e64bb33fe
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-04-24 20:58:37 +0000

    finished useState

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -3,16 +3,16 @@ import React, {useState} from 'react'
 import '/src/styles/trafficLight.css'
 
 export const TrafficLight = () => {
-    const [colorActivo, setColorActivo] = useState("verde")
+    const [colorActivo, setColorActivo] = useState("")
 
     return (<>
         
         <div className='container'>
             <div className="bg-dark mx-5" id='palo' style={{maxWidth: "3em"}}></div>
             <div className="container bg-dark p-4 mx-0 mb-4 d-flex flex-column justify-content-center" style={{maxWidth: "10em"}}>
-            <   div className="rojo"></div>
-                <div className="amarillo"></div>
-                <div className={"verde"+" "+`${colorActivo == 'verde' ? "brillo" : " "}`}></div>
+            <   div onClick={() => setColorActivo("rojo")} className={"rojo"+" "+`${colorActivo == 'rojo' ? "brillo" : " "}`}></div>
+                <div onClick={() => setColorActivo("amarillo")} className={"amarillo"+" "+`${colorActivo == 'amarillo' ? "brillo" : " "}`}></div>
+                <div onClick={() => setColorActivo("verde")} className={"verde"+" "+`${colorActivo == 'verde' ? "brillo" : " "}`}></div>
             </div>
         </div>
 
